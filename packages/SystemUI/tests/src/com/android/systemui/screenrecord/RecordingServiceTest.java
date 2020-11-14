bf0d70e4983db714ee2880ecf31bd91597bcd851
ezio84 <brabus84@gmail.com>
2020-11-14 22:23:23 +0100

    Screenrecord: add blinking stop dot and low quality options
    We could want to stop the recording without expanding the notification
    panel and show sensitive info.
    Long pressing on the red dot moves it to the opposite side,
    in case the user wants not to cover specific elements on the screen.
    
    Add also a low quality option that still gives a good recording
    reducing a lot the final file size (useful when sharing the screenrecord
    on data connections).
    
    old commits ref:
    https://github.com/ezio84/abc_frameworks_base/commit/6092d9ca8b0949d64c1b87d6cd53d66daf002e14
    https://github.com/ezio84/abc_frameworks_base/commit/b50c7caf8b46c731093b11f5647028a23346f937
    https://github.com/ezio84/abc_frameworks_base/commit/91c00e8fb70cb17b8658678bf476452f96cd44d1
    
    Change-Id: If413241c3fa533650690cf3b7df5c05fb2f8c8ed

--- packages/SystemUI/tests/src/com/android/systemui/screenrecord/RecordingServiceTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/screenrecord/RecordingServiceTest.java
@@ -139,7 +139,8 @@ public class RecordingServiceTest extends SysuiTestCase {
 
     @Test
     public void testLogStartFullScreenRecording() {
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         verify(mUiEventLogger, times(1)).log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
@@ -149,7 +150,8 @@ public class RecordingServiceTest extends SysuiTestCase {
     public void testLogStartPartialRecording() {
         MediaProjectionCaptureTarget target =
                 new MediaProjectionCaptureTarget(new LaunchCookie(), 12345);
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, target);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, target, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         verify(mUiEventLogger, times(1)).log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
@@ -182,7 +184,8 @@ public class RecordingServiceTest extends SysuiTestCase {
         // When the screen recording does not start properly
         doThrow(new RuntimeException("fail")).when(mScreenMediaRecorder).start();
 
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         assertUpdateState(false);
