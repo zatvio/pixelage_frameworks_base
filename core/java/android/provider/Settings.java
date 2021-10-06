49085da5ca69f04a6199490e0282fbd481941b47
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-05 20:05:42 -0700

    SystemUI: Show daily data usage in QS footer
    Squashed:
    
     * Refactor existing build text view
     * SettingsLib: Add method to get daily data usage
     * SystemUI: Show wifi usage in QS footer when available
     * SystemUI: Move QS page indicator to the right
     * SystemUI: Toggle data usage view on QS footer [1/2]
     * SystemUI: Fix daily data view in QS footer
     * SystemUI: Avoid NPE in QS footer data usage
    
    SystemUI: Remove build version from qs footer
    
    Change-Id: I7bb8f1422765d9e09bd50b43fa1e9849fe006344
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/5922
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-by: Nitish <nitish.98.sharma@gmail.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/7157
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: travarilo <travarilo@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ifb5f7c74e2f1d07ab9c89c2f7f6ea95151203ff0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6505,6 +6505,13 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Whether to show data usage in qs footer
+         * 
+         * @hide
+         */
+        public static final String QS_FOOTER_DATA_USAGE = "qs_footer_data_usage";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
