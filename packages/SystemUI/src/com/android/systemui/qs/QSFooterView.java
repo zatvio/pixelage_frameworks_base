5d9022f787c8e56e79e884e5f4306af792bd9fb0
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 08:17:07 +0800

    SystemUI: Improve QS data usage info
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic415ff1b32f69b925b8bf603ef6430ac8ad707bb
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -205,7 +205,7 @@ public class QSFooterView extends FrameLayout {
     }
 
     private void updateUsageTextResources() {
-        FontSizeUtils.updateFontSizeFromStyle(mUsageText, R.style.TextAppearance_QS_Status_Build);
+        FontSizeUtils.updateFontSizeFromStyle(mUsageText, R.style.TextAppearance_QS_Status_DataUsage);
     }
 
     private void updateFooterAnimator() {
--- packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
@@ -18,6 +18,7 @@ package com.android.systemui.qs;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Intent;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
@@ -41,6 +42,7 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
 
     private final UserTracker mUserTracker;
     private final QSPanelController mQsPanelController;
+    private final TextView mUsageText;
     private final PageIndicator mPageIndicator;
     private final View mEditButton;
     private final FalsingManager mFalsingManager;
@@ -64,6 +66,7 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
 
         mPageIndicator = mView.findViewById(R.id.footer_page_indicator);
         mEditButton = mView.findViewById(android.R.id.edit);
+        mUsageText = mView.findViewById(R.id.build);
     }
 
     @Override
@@ -74,6 +77,12 @@ public class QSFooterViewController extends ViewController<QSFooterView> impleme
             }
             mActivityStarter
                     .postQSRunnableDismissingKeyguard(() -> mQsPanelController.showEdit(view));
+        });
+        mUsageText.setOnClickListener(view -> {
+            Intent nIntent = new Intent(Intent.ACTION_MAIN);
+            nIntent.setClassName("com.android.settings",
+                    "com.android.settings.Settings$DataUsageSummaryActivity");
+            mActivityStarter.startActivity(nIntent, true /* dismissShade */);
         });
         mQsPanelController.setFooterPageIndicator(mPageIndicator);
         mView.updateEverything();
